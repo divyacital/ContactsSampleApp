d1192659ff4f5e59f12b1a733bb326eceb07d196
Divya <divya@cital.co>
2022-11-27 17:07:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/contactssampleapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.contactssampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val myWebView: WebView = findViewById(R.id.webview)
+    myWebView.settings.javaScriptEnabled = true
+    myWebView.loadUrl("https://contact-picker.glitch.me/")
+  }
+}
